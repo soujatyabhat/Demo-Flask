4deeaf7ca9bbbc6b955abe6254e3be09b10e421c
soujatyabhat <soujatya.bhat2000@gmail.com>
2022-09-17 20:58:23 +0530

    New App

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Web App with Python Flask!'
 
-app.run(host='0.0.0.0')
+@app.route('/', methods=['GET'])
+def helloworld():
+	if(request.method == 'GET'):
+		data = {"data": "Hello World"}
+		return jsonify(data)
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
