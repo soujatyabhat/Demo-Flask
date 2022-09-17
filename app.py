7bc8db48601cac5bf95bfc0c30993d5f0996177f
soujatyabhat <soujatya.bhat2000@gmail.com>
2022-09-17 21:02:25 +0530

    New

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask, jsonify, request
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 18 19:33:15 2022
+@author: souja
+"""
+
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def main():
+    return "Welcome to my home 1"
+    
+@app.route("/how_are_you")
+def hello():
+    return "I am Good. What about u"
 
-@app.route('/', methods=['GET'])
-def helloworld():
-	if(request.method == 'GET'):
-		data = {"data": "Hello World"}
-		return jsonify(data)
-
-
-if __name__ == '__main__':
-	app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
