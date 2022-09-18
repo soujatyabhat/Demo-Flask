1fcacc1cd9ea3ed7b666e98fb29a67ae86d00723
Soujatya Bhattacharya <65819358+soujatyabhat@users.noreply.github.com>
2022-09-18 13:17:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 18 19:33:15 2022
-@author: souja
-"""
-
-import os
-from flask import Flask
-
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return "Welcome to my home 1"
-    
-@app.route("/how_are_you")
-def hello():
-    return "I am Good. What about u"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+@app.route('/success/<name>')
+def success(name):
+	return 'welcome %s' % name
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+	if request.method == 'POST':
+		user = request.form['nm']
+		return redirect(url_for('success', name=user))
+	else:
+		user = request.args.get('nm')
+		return redirect(url_for('success', name=user))
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
